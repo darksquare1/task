ff5ea0859066183dd3ab7e66e305a96a8f99bb48
depressedkid <nekit05@list.ru>
2024-09-25 20:49:11 +1000

    added RetrieveUpdateDestroyCarView

--- cars_api/urls.py
+++ cars_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cars_api.views import ListCarsApiView
+from cars_api.views import ListCarsApiView, RetrieveUpdateDestroyCarView
 
 urlpatterns = [
-    path('cars/', ListCarsApiView.as_view())
+    path('cars/', ListCarsApiView.as_view()),
+    path('cars/<int:pk>/',RetrieveUpdateDestroyCarView.as_view()),
 ]
--- cars_api/views.py
+++ cars_api/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+
 from cars.models import Car
 from cars_api.serializers import CarSerializer
 
 
-class ListCarsApiView(generics.ListAPIView): # представление для отображения машин в json по гет запросу
+class ListCarsApiView(generics.ListAPIView):
+    """
+    Представление для отображения машин в json по get запросу
+    """
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
+
+class RetrieveUpdateDestroyCarView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Представление для получения конкретной машины, обновления и удаления (get, put, delete) соответственно
+    """
+    serializer_class = CarSerializer
+    queryset = Car.objects.all()
+
+    def put(self, request, *args, **kwargs):
+        if request.user != self.get_object().owner: # если запрос делает не владелец записи, то отвечаем запретом
+            return Response({'detail': 'Вы не создатель данной записи'}, status=403)
+        return super().put(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        if request.user != self.get_object().owner: # если запрос делает не владелец записи, то отвечаем запретом
+            return Response({'detail': 'Вы не создатель данной записи'}, status=403)
+        return super().delete(request, *args, **kwargs)
